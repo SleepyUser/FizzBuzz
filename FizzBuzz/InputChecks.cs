e404bac6933fdaea5316d3323e2a250fa5497140
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-05 16:04:51 +0100

    Fizzbuzz completed up Command Line options

---
+++ FizzBuzz/InputChecks.cs
@@ -0,0 +1,129 @@
+﻿namespace FizzBuzz;
+
+public static class InputChecks
+{
+    /// <summary>
+    /// Asks the user to enter an operator, and repeats until they enter a valid input
+    /// </summary>
+    /// <returns>char: either +, -, /, *</returns>
+    public static char getValidOperator()
+    {
+        Console.WriteLine("Please type an operator (*, /, +, -)");
+        return GetValidChar(new[] { '+', '-', '/', '*' }, true);
+    }
+
+    /// <summary>
+    /// Asks the user to enter a double, and repeatedly asks until a valid answer is entered
+    /// </summary>
+    /// <returns>the successfully entered double</returns>
+    public static double getValidDouble()
+    {
+        while (true)
+            try
+            {
+                return Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+    }
+
+    /// <summary>
+    /// Asks the user to enter Y or N (not case sensitive) and returns the corresponding bool
+    /// </summary>
+    /// <returns>True if user entered Y, False if N</returns>
+    public static bool getYN()
+    {
+        var input = "";
+        while (true)
+        {
+            Console.WriteLine("Please enter Y or N.");
+            input = Console.ReadLine() ?? string.Empty;
+            if (input == "Y" || input == "y") return true;
+
+            if (input == "N" || input == "n") return false;
+            Console.WriteLine("Invalid input.");
+        }
+    }
+
+    /// <summary>
+    /// Asks the user to enter a character and checks it against the parameter list of valid characters
+    /// Users will be asked to repeatedly enter a character until one on the list is entered.
+    /// </summary>
+    /// <param name="validChars">An array of chars considered valid by the check. If Case Sensitive is true,
+    /// Array will be converted to upper case.</param>
+    /// <param name="caseSensitive"> False by default, is the list of valid chars case sensitive?</param>
+    /// <returns></returns>
+    public static char GetValidChar(char[] validChars, bool caseSensitive = false)
+    {
+        var input = 'a'; // a used only to initalise
+        if (!caseSensitive) //If not case sensitive, convert all to upper
+            for (var i = 0; i < validChars.Length; i++)
+                validChars[i] = char.ToUpper(validChars[i]);
+        while (true)
+        {
+            input = GetValidChar();
+            if (!caseSensitive) //If not case sensitive convert input to upper
+                input = char.ToUpper(input);
+            if (validChars.Contains(input)) return input;
+            Console.WriteLine("Invalid Input, please enter only a valid character.");
+        }
+    }
+
+    public static char GetValidChar()
+    {
+        var input = '0';
+        while (!char.TryParse(Console.ReadLine() ?? string.Empty, out input))
+            Console.WriteLine("Invalid input, please enter a valid character.");
+        return input;
+    }
+
+    /// <summary>
+    /// Asks the user to enter a date or time and returns the parsed datetime.
+    /// Keeps asking if it cannot parse the input
+    /// </summary>
+    /// <returns>The parsed datetime</returns>
+    public static DateTime getValidDateTime()
+    {
+        var input = DateTime.Now;
+        while (!DateTime.TryParse(Console.ReadLine() ?? string.Empty, out input)) Console.WriteLine("Invalid input.");
+
+        return input;
+    }
+
+    /// <summary>
+    /// Asks the user to enter an int, and repeatedly asks until a valid answer is entered
+    /// </summary>
+    /// <returns>the successfully entered int</returns>
+    public static int getValidInt(bool posOnly = false, bool nonZero = false)
+    {
+        var input = 0;
+        while (true)
+        {
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input, please try again.");
+                continue;
+            }
+
+            if (nonZero && input == 0)
+            {
+                Console.WriteLine("Zero is not an acceptable input here.");
+                continue;
+            }
+
+            if (posOnly && input < 0)
+            {
+                Console.WriteLine("Negative numbers are not accepted here.");
+                continue;
+            }
+
+            return input;
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,218 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Data;
+
+namespace FizzBuzz;
+
+[Flags]
+public enum Rules
+{
+    None    = 0b_0000_0000,
+    Fizz    = 0b_0000_0001,
+    Buzz    = 0b_0000_0010,
+    Bang    = 0b_0000_0100,
+    Bong    = 0b_0000_1000,
+    Fezz    = 0b_0001_0000,
+    Reverse = 0b_0010_0000,
+    All = Fizz | Buzz | Bang | Bong | Fezz | Reverse
+}
+
+class FizzBuzz
+{
+
+    static void Main(string[] args)
+    {
+        var max = ChangeMaxNum();
+
+        Console.WriteLine("Would you like to enable any rules?");
+        InputChecks.getYN();
+        Rules selectedRules = RuleChooser();
+        for (int i = 1; i <= max; i++)
+        {
+            new FizzBuzzNum(i, selectedRules);
+        }
+    }
+
+    private static int ChangeMaxNum()
+    {
+        int max = 100;
+        Console.WriteLine("Please enter a number to FizzBuzz up to.");
+        max = InputChecks.getValidInt();
+        return max;
+    }
+
+    private static Rules RuleChooser()
+    {
+        Rules selectedRules = Rules.None;
+        int input = 0;
+        do
+        {
+            Console.WriteLine("Type a number to toggle that rule. (Or disable/enable all rules)");
+            input = InputChecks.getValidInt(posOnly:true);
+            if (input is (int)Rules.None or (int)Rules.All)
+            {
+                selectedRules = (Rules)input;
+            }
+            else if (input < (int)Rules.All)
+            {
+                selectedRules ^= (Rules)input;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input.");
+            }
+            Console.WriteLine("Finished? Y/N");
+        } while (!InputChecks.getYN());
+
+        return selectedRules;
+    }
+
+    private class FizzBuzzNum
+    {
+        private List<string> _output = new List<string>();
+        private bool _numexists = true;
+        private int _number = 0;
+        private Rules _selectedRules;
+
+        public FizzBuzzNum(int number, Rules selectedRules)
+        {
+            _number = number;
+            _selectedRules = selectedRules;
+
+            _output.Add(number.ToString());
+            
+            //fizz = (number % 3 == 0);
+            if (checkRule(Rules.Fizz))
+            {
+                Fizz();
+            }
+
+            //buzz = (number % 5 == 0);
+            if (checkRule(Rules.Buzz))
+            {
+                Buzz();
+            }
+            
+            //bang = (number % 7 == 0);
+            if (checkRule(Rules.Bang))
+            {
+                Bang();
+            }
+            
+            //bong = (number % 11 == 0);
+            if (checkRule(Rules.Bong))
+            {
+                Bong();
+            }
+
+            //fezz = (number % 13 == 0);
+            if (checkRule(Rules.Fezz))
+            {
+                Fezz();
+            }
+            
+            //reverse = (number % 17 == 0);
+            if (checkRule(Rules.Reverse))
+            {
+                Reverse();
+            }
+            
+            Console.WriteLine(string.Concat(_output));
+        }
+
+        private bool checkRule(Rules toCheck)
+        {
+            return (_selectedRules & toCheck) == toCheck;
+        }
+
+        private void Reverse()
+        {
+            if (_number % 17 == 0) //Reverse
+            {
+                _output.Reverse();
+            }
+        }
+
+        private void Fezz()
+        {
+            if (_number % 13 == 0) //Fezz
+            {
+                if (_numexists) //if there is a number, replace it
+                {
+                    ReplaceNumber("Fezz");
+                }
+                else // if there is no number, insert before first "B" word. Else just append
+                {
+                    try
+                    {
+                        _output.Insert(_output.FindIndex(x => x[0] is 'B' or 'b'), "Fezz");
+                    }
+                    catch
+                    {
+                        _output.Append("Fezz");
+                    }
+                }
+            }
+        }
+
+        private void Bong()
+        {
+            if (_number % 11 == 0) //Bong
+            {
+                _output.Clear();
+                _output.Add("Bong");
+            }
+        }
+
+        private void Bang()
+        {
+            if (_number % 7 == 0) //Bang
+            {
+                if (_output.Contains("Fizz") || _output.Contains("Buzz"))
+                {
+                    _output.Add("Bang");
+                }
+                else if (_numexists)
+                {
+                    ReplaceNumber("Bang");
+                }
+            }
+        }
+
+        private void Buzz()
+        {
+            if (_number % 5 == 0) //Buzz
+            {
+                if (_numexists)
+                {
+                    ReplaceNumber("Buzz");
+                }
+                else
+                {
+                    _output.Add("Buzz");
+                }
+            }
+        }
+
+        private void Fizz()
+        {
+            if (_number % 3 == 0) //Fizz
+            {
+                if (_numexists)
+                {
+                    ReplaceNumber("Fizz");
+                }
+                else
+                {
+                    _output.Add("Fizz");
+                }
+            }
+        }
+
+        private void ReplaceNumber(string replacement)
+        {
+            _output[_output.FindIndex(x => x == _number.ToString())] = replacement;
+            _numexists = false;
+        }
+    }
+}
